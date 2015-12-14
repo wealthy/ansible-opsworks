a0c7c30ae6153c38a352295436cb5162c57214ac
Deepak Aggarwal <deepak@Deepaks-MacBook-Air.local>
2015-12-14 15:41:50 +0530

    Temporary commit

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -43,7 +43,7 @@ extra_vars['deploy']  = node['deploy']
 # Finding out repositoru url
 #extra_vars['ansible']['source_code_url']  = node['deploy'][app]['scm']['repository']
 
-#extra_vars['environment_variables'] = node['deploy'][app]['environment_variables'] 
+extra_vars['environment_variables'] = node['deploy'][app]['environment_variables'] 
 folder = node['ansible']['folder']
 extra_vars['node'] = node
 
